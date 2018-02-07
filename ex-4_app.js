7561d3d816fc9001211cc1c81654372d8442eb56
Gabriel Armstrong <gabriel.armstrong@lepisc.com>
2018-02-07 10:13:00 -0500

    début ex-4

--- ex-4_app.js
+++ ex-4_app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
 app.get('/html/04_form.html', (req, res) => {
- console.log(__dirname);
- res.sendFile( __dirname + "/" + "04_form.html" );
+	console.log(__dirname);
+	res.sendFile( __dirname + "/" + "04_form.html" );
 })
 
 app.get('/', (req, res) => {
@@ -13,22 +14,20 @@ app.get('/', (req, res) => {
 
 app.get('/traiter_get', (req, res) => {
  // Preparer l'output en format JSON
-
-console.log('la route /traiter_get')
-
+	console.log('la route /traiter_get')
 // on utilise l'objet req.query pour récupérer les données GET
- reponse = {
- prenom:req.query.prenom,
- nom:req.query.nom
- };
-console.log(reponse);
- res.end(JSON.stringify(reponse));
+	reponse = {
+		prenom:req.query.prenom,
+		nom:req.query.nom
+	};
+
+	console.log(reponse);
+	res.end(JSON.stringify(reponse));
 })
 
 let server = app.listen(8081, () => {
- let host = server.address().address
- let port = server.address().port
+	let host = server.address().address
+	let port = server.address().port
  
- console.log("Exemple l'application écoute sur http://%s:%s", host, port)
-
+ 	console.log("Exemple l'application écoute sur http://%s:%s", host, port)
 })
